5b15c4689721e88c1fe6d432b7fa128250273a46
Ricardo-Pacheco <ricksouzapv@gmail.com>
2024-01-14 15:43:26 -0300

    tentando arruma a imagem do ps5

--- product.js
+++ product.js
@@ -5,5 +5,5 @@ const productsList =
     { name: "PlayStation 3 Pro", price: 1500.00, src: "./img/playstation3Pro.png", alt: "playstation3-pro-img"},
     { name: "PlayStation 4 SLim", price: 2370.00, src: "./img/Playstation4slim.png", alt: "playstation4-slim-img"},
     { name: "PlayStation 4 Pro", price: 3270.00, src: "./img/PlayStation4Pro.png", alt: "playstation4-pro-img"},
-    { name: "PlayStation 5 Slim", price: 4800.00, src: "./img/ps5slim.png", alt: "playstation5-slim-img"}
+    { name: "PlayStation 5 Slim", price: 4800.00, src: "./img/ps5slim.png", alt: "ps5-slim-img"}
 ]
