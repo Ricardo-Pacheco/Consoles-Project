1862ad9c5b13725188505d992dbddf15cda08c39
Ricardo-Pacheco <ricksouzapv@gmail.com>
2024-01-14 15:35:39 -0300

    mudança nas imagens

--- product.js
+++ product.js
@@ -1,7 +1,7 @@
 const productsList = 
 [
     { name: "PlayStation 2 SLim", price: 470.00, src: "./img/playstation2.png", alt: "playstation2-img"},
-    { name: "PlayStation 3 SLim", price: 850.00, src: "./img/playstation3slim.png", alt: "playstation3-slim-img"},
+    { name: "PlayStation 3 SLim", price: 850.00, src: "./img/PS3Slim.png", alt: "playstation3-slim-img"},
     { name: "PlayStation 3 Pro", price: 1500.00, src: "./img/playstation3Pro.png", alt: "playstation3-pro-img"},
     { name: "PlayStation 4 SLim", price: 2370.00, src: "./img/Playstation4slim.png", alt: "playstation4-slim-img"},
     { name: "PlayStation 4 Pro", price: 3270.00, src: "./img/PlayStation4Pro.png", alt: "playstation4-pro-img"},
