ecd98696d12c9f0f8be75f180582db8f166c46e5
Ricardo-Pacheco <ricksouzapv@gmail.com>
2024-01-15 20:58:21 -0300

    alteraçoes no product.js e style.css

--- product.js
+++ product.js
@@ -1,9 +1,9 @@
 const productsList = 
 [
-    { name: "PS2 SLim", price: 470.00, src: "./img/playstation2.png", alt: "playstation2-img"},
-    { name: "PS3 SLim", price: 850.00, src: "./img/PS3Slim.png", alt: "playstation3-slim-img"},
-    { name: "PS3 Pro", price: 1500.00, src: "./img/playstation3Pro.png", alt: "playstation3-pro-img"},
-    { name: "PS4 SLim", price: 2370.00, src: "./img/Playstation4slim.png", alt: "playstation4-slim-img"},
-    { name: "PS4 Pro", price: 3270.00, src: "./img/PlayStation4Pro.png", alt: "playstation4-pro-img"},
-    { name: "PS5 Slim", price: 4800.00, src: "./img/ps5.png", alt: "ps5-img"}
+    { name: "Plastation 2 Slim", price: 470.00, src: "./img/playstation2.png", alt: "playstation2-img"},
+    { name: "Plastation 3 Slim", price: 850.00, src: "./img/PS3Slim.png", alt: "playstation3-slim-img"},
+    { name: "Plastation 3 Pro", price: 1500.00, src: "./img/playstation3Pro.png", alt: "playstation3-pro-img"},
+    { name: "Plastation 4 Slim", price: 2370.00, src: "./img/Playstation4slim.png", alt: "playstation4-slim-img"},
+    { name: "Plastation 4 Pro", price: 3270.00, src: "./img/PlayStation4Pro.png", alt: "playstation4-pro-img"},
+    { name: "Plastation 5 Slim", price: 4800.00, src: "./img/ps5.png", alt: "ps5-img"}
 ]
